46f29912d5f23a0b101ace567edcffad7027c1cd
Bullobily <bullobily@gmail.com>
2021-05-02 13:59:52 +0200

    up /tp with rotation

--- src/main/java/fr/olympa/olympacreatif/commandblocks/commands/CmdTeleport.java
+++ src/main/java/fr/olympa/olympacreatif/commandblocks/commands/CmdTeleport.java
@@ -12,53 +12,60 @@ import fr.olympa.olympacreatif.commandblocks.commands.CbCommand.CommandType;
 import fr.olympa.olympacreatif.plot.Plot;
 
 public class CmdTeleport extends CbCommand {
-	
-	private List<Location> tpPoints = new ArrayList<Location>();
+
+	private Location tpLoc = null;
 	
 	public CmdTeleport(CommandSender sender, Location loc, OlympaCreatifMain plugin, Plot plot, String[] args) {
 		super(CommandType.teleport, sender, loc, plugin, plot, args);
-	
-		switch(args.length) {
-		case 1:
+		
+		if (args.length == 1) {
 			if (sender instanceof Entity) {
 				targetEntities.add((Entity) sender);
-				for (Entity e : parseSelector(args[0], false))
-					tpPoints.add(e.getLocation());	
+				tpLoc = getLocFromSelector(args[0]);	
 			}
-			break;
-		case 2:
-			targetEntities = parseSelector(args[0], false);
-			for (Entity e : parseSelector(args[1], false))
-				tpPoints.add(e.getLocation());
-			break;
-		case 3:
-			if (sender instanceof Entity) {
-				targetEntities.add((Entity) sender);
-				Location loc1 = args.length == 5 ? parseLocation(args[0], args[1], args[2], args[3], args[4]) : parseLocation(args[0], args[1], args[2]);
-				if (loc1 != null)
-					tpPoints.add(loc1);	
-			}
-			break;
-		case 4:
-			targetEntities = parseSelector(args[0], false);
-			Location loc2 = args.length == 5 ? parseLocation(args[0], args[1], args[2], args[3], args[4]) : parseLocation(args[0], args[1], args[2]);
-			if (loc2 != null)
-				tpPoints.add(loc2);
-			break;
 		}
+		
+		else if (args.length == 2) {
+			targetEntities = parseSelector(args[0], false);
+			tpLoc = getLocFromSelector(args[1]);
+		} 
+		
+		else if (args.length == 3) {
+			if (sender instanceof Entity) {
+				targetEntities.add((Entity) sender);
+				tpLoc = parseLocation(args[0], args[1], args[2]);	
+			}
+		}
+		
+		else if (args.length == 4) {
+			targetEntities = parseSelector(args[0], false);
+			tpLoc = parseLocation(args[1], args[2], args[3]);
+		}
+		
+		else if (args.length == 5) {
+			if (sender instanceof Entity) {
+				targetEntities.add((Entity) sender);
+				tpLoc = parseLocation(args[0], args[1], args[2], args[3], args[4]);	
+			}
+		}
+		
+		else if (args.length == 6) {
+			targetEntities = parseSelector(args[0], false);
+			tpLoc = parseLocation(args[1], args[2], args[3], args[4], args[5]);
+		}
+	}
+	
+	private Location getLocFromSelector(String selector) {
+		List<Entity> list = parseSelector(selector, false);
+		return list.size() > 0 ? list.get(0).getLocation() : null;
 	}
 	
 	@Override
 	public int execute() {
-		if (tpPoints.size() == 0)
+		if (tpLoc == null)
 			return 0;
 		
-		int i = 0;
-		
-		for (Entity e : targetEntities) {
-			i++;
-			e.teleport(tpPoints.get(0));
-		}
-		return i;
+		targetEntities.forEach(ent -> ent.teleport(tpLoc));
+		return targetEntities.size();
 	}
 }
